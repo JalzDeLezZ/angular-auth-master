54cede6a2480a7717422a299f9be45aaa055577d
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-11-22 08:46:35 -0500

    [add] (register)

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,17 +4,22 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   apiUrl = environment.api_url;
 
-  constructor(
-    public http: HttpClient
-  ) { }
+  constructor(public http: HttpClient) {}
 
   login(email: string, password: string) {
     return this.http.post(`${this.apiUrl}/auth/login`, { email, password });
+  }
+
+  register(name: string, email: string, password: string) {
+    return this.http.post(`${this.apiUrl}/auth/register`, {
+      name,
+      email,
+      password,
+    });
   }
 }
